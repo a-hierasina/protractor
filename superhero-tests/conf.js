92792ebb63961f010062a3b797d49f4c05c869cf
Anna <anna.hierasina@gmail.com>
2019-07-16 13:41:42 +0300

    add reports

--- superhero-tests/conf.js
+++ superhero-tests/conf.js
@@ -1,3 +1,13 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+    dest: 'target/screenshots',
+    filename: 'my-report.html',
+    reportOnlyFailedSpecs: false,
+    captureOnlyFailedSpecs: true,
+    reportTitle: "Superhero Test Results"
+});
 exports.config = {
     framework: 'jasmine2',
 
@@ -12,6 +22,35 @@ exports.config = {
     ],
 
     jasmineNodeOpts: {
-        showColors: true
+        showColors: true,
+        silent: true,
+        defaultTimeoutInterval: 360000,
+        print: function () {
+        }
+    },
+
+    // Setup the report before any tests start
+    beforeLaunch: function () {
+        return new Promise(function (resolve) {
+            reporter.beforeLaunch(resolve);
+        });
+    },
+    // Close the report after all tests finish
+    afterLaunch: function (exitCode) {
+        return new Promise(function (resolve) {
+            reporter.afterLaunch(resolve.bind(this, exitCode));
+        });
+    },
+    logLevel: 'WARN',
+    onPrepare: function () {
+        jasmine.getEnv().addReporter(reporter);
+        jasmine.getEnv().addReporter(new SpecReporter({
+            spec: {
+                displayStacktrace: true
+            },
+            summary: {
+                displayDuration: false
+            }
+        }));
     }
 };
